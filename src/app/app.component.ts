108340082a206f3e9f0f78c8e894a6fcd36f30b7
phamlananh1 <phamlananh12011995@gmail.com>
2019-03-20 17:06:18 +0700

    file

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 's6-Countdown-timer';
+  countdownMsg = '';
+  countdownAliasMsg = '';
+
+  finishCountdown() {
+    this.countdownMsg = 'Finished!';
+  }
+
+  endCountdown() {
+    this.countdownAliasMsg = 'Ended!';
+  }
+
+  // onRateChange(value) {
+  //   console.log(value);
+  //
+  // }
+}
